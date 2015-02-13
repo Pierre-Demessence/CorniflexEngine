d597eda11bb2fd75d95cc3ab25296fad7c2022e2
Pierre Demessence <pierredemessence@gmail.com>
2015-02-13 16:40:26 +0100

    Unit Tests for EventManager

--- test/EventManagerTest.cpp
+++ test/EventManagerTest.cpp
@@ -4,49 +4,86 @@
 #include	"corniflex/Event.hh"
 #include	<iostream>
 
-class		FakeSystem {
+class		FakeEvent1 : public corniflex::Event {
 public:
-  void		function(corniflex::Event *e) {
-    (void)e;
-    std::cout << "FakeSystem Event Handler" << std::endl;
-  }
+  int	_id = 42;
+  int getID() { return (_id); }
 };
 
-class		FakeEvent : public corniflex::Event
-{
+class		FakeEvent2 : public corniflex::Event {
+public:
+  int	_id = 10;
+  int getID() { return (_id); }
+};
 
+class		FakeSystem {
+public:
+  void		function(corniflex::Event *) {}
 };
 
 BOOST_AUTO_TEST_SUITE(EventManagerTesting)
 
-BOOST_AUTO_TEST_CASE(EventManagerTest)
+BOOST_AUTO_TEST_CASE(EventManagerTestHandlers)
 {
   corniflex::EventManager	manager;
   FakeSystem			system;
 
-  manager.addHandler(corniflex::Event(), std::bind(&FakeSystem::function, &system, std::placeholders::_1));
-  manager.addHandler(corniflex::Event(), [] (corniflex::Event *e) {
-    (void)e;
-    std::cout << "Lambda Event Handler" << std::endl;
-  });
-  manager.addHandler(FakeEvent(), [] (corniflex::Event *e) {
-    (void)e;
-    std::cout << "Lambda FakeEvent Handler" << std::endl;
-  });
   manager.setSynchronous(true);
-  std::cout << "Event Fired" << std::endl;
-  manager.sendEvent(new corniflex::Event(), [] (corniflex::Event *e) {
-    (void)e;
-    std::cout << "Event Callback" << std::endl;
+  manager.addHandler(FakeEvent1(), std::bind(&FakeSystem::function, &system, std::placeholders::_1));
+  manager.sendEvent(new FakeEvent1());
+  BOOST_CHECK(manager.getNbProcessedEvent() == 1);
+  manager.addHandler(FakeEvent1(), [] (corniflex::Event *) {});
+  manager.sendEvent(new FakeEvent1());
+  BOOST_CHECK(manager.getNbProcessedEvent() == 3);
+}
+
+BOOST_AUTO_TEST_CASE(EventManagerTestAsynchronous)
+{
+  corniflex::EventManager	manager;
+
+  manager.setSynchronous(false);
+  manager.sendEvent(new FakeEvent1());
+  BOOST_CHECK(manager.getNbProcessedEvent() == 0);
+  manager.addHandler(FakeEvent1(), [] (corniflex::Event *) {});
+  manager.sendEvent(new FakeEvent1());
+  BOOST_CHECK(manager.getNbProcessedEvent() == 0);
+  manager.processLastEvent();
+  BOOST_CHECK(manager.getNbProcessedEvent() == 1);
+  manager.processLastEvent();
+  BOOST_CHECK(manager.getNbProcessedEvent() == 2);
+}
+
+BOOST_AUTO_TEST_CASE(EventManagerTestCallback)
+{
+  corniflex::EventManager	manager;
+
+  bool	b = false;
+  manager.setSynchronous(true);
+  manager.sendEvent(new FakeEvent1(), [] (corniflex::Event *) {
+    BOOST_FAIL("Should not call calback");
   });
-  std::cout << "FakeEvent Fired" << std::endl;
-  manager.sendEvent(new FakeEvent(), [] (corniflex::Event *e) {
-    (void)e;
-    std::cout << "FakeEvent Callback" << std::endl;
+  manager.addHandler(FakeEvent1(), [] (corniflex::Event *) {
   });
-  manager.processFirstEvent();
-  manager.processFirstEvent();
-  manager.processFirstEvent();
+  manager.sendEvent(new FakeEvent1(), [&b] (corniflex::Event *) {
+    b = true;
+  });
+  BOOST_CHECK(b);
+}
+
+BOOST_AUTO_TEST_CASE(EventManagerTestEventsType)
+{
+  corniflex::EventManager	manager;
+
+  manager.setSynchronous(true);
+  manager.addHandler(FakeEvent1(), [] (corniflex::Event *e) {
+    BOOST_CHECK(dynamic_cast<FakeEvent1 *>(e) != NULL);
+  });
+  manager.addHandler(FakeEvent2(), [] (corniflex::Event *e) {
+    BOOST_CHECK(dynamic_cast<FakeEvent1 *>(e) == NULL);
+  });
+
+  manager.sendEvent(new FakeEvent1());
+  manager.sendEvent(new FakeEvent2());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
